85dc641517a1e68bf5f72cef0f6ad80f3223761c
Fides <mwendafides613@gmail.com>
2025-09-11 20:08:57 +0300

    Initial commit: basic_operations script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 12
+number2 = 8
+
+print("Addition of", number1, "and", number2, "is", number1 + number2)
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)
